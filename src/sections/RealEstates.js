7c61e3ac431700eb4600d5c300be44d9464b8e2f
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-09-01 00:18:17 +0800

    Remove the green background

--- src/sections/RealEstates.js
+++ src/sections/RealEstates.js
@@ -5,8 +5,8 @@ const manrope = Manrope({ subsets: ['latin'] })
 
 function RealEstates() {
   return (
-    <section className={`h-screen w-screen bg-green-300 ${manrope.className}`}>
-        <div className='flex justify-center w-full'>
+    <section className={`h-screen w-screen ${manrope.className}`}>
+        <div className='flex justify-center w-full mt-[25%]'>
             <h1 className='flex justify-center items-center w-[90%] md:w-[70%] lg:w-[40%] xl:w-[30%] font-extrabold text-6xl leading-[135%] tracking-[0.6px]'>Our Best Estate,  are Modern!</h1>
         
 
