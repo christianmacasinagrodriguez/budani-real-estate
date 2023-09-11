702330544e7b830ec055798be3e9a452e877696e
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-09-11 22:58:06 +0800

    Change img to Image

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -29,7 +29,7 @@ function Feature() {
         <div className='h-fit w-screen grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-y-[137px]'>
             <div className='h-[257px] md:h-[456px] lg:h-[456px] w-[90%] bg-[#6663E9] rounded-l-[70px] flex flex-row justify-start items-end self-center justify-self-end order-2 md:order-2 lg:order-2 shadow-[0px_4px_60px_0px_rgba(0, 0, 0, 0.05)]'>  
                 <div className='w-[292px] h-[292px] md:w-[518px] md:h-[518px] lg:w-[518px] lg:h-[518px] relative z-[1] mb-[67px]'>
-                    <img className='w-full h-full object-cover object-center rounded-[50px] ' src='https://images.unsplash.com/photo-1582407947304-fd86f028f716?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1896&q=80' alt='featured-image'/>     
+                    <Image className='w-full h-full object-cover object-center rounded-[50px] ' src='https://images.unsplash.com/photo-1582407947304-fd86f028f716?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1896&q=80' alt='featured-image'/>     
                     <div className='w-[238px] h-[238px] md:w-[422px] md:h-[422px] lg:w-[422px] lg:h-[422px]  absolute bottom-0 -mr-[27px] z-[-1] -mb-[27px] right-0 bg-white rounded-[65px] shadow-[0px_4px_60px_0px_rgba(0, 0, 0, 0.05)]'></div>                 
                 </div>                    
             </div>
