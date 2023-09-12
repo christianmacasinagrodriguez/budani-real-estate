41fe9d60700f341a56805b6d0149de644f1bc8e4
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-09-12 23:01:22 +0800

    Change the newsletter section horizontal padding

--- src/sections/Newsletter.js
+++ src/sections/Newsletter.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Newsletter() {
   return (
-    <section className='w-screen h-fit grid place-items-center pt-[200px] overflow-x-hidden'>
+    <section className='w-screen h-fit grid place-items-center py-[200px] overflow-x-hidden'>
         <NewsLetterCTA />
         
     </section>
