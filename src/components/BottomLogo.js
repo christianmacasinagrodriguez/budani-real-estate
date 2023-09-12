2a8b3b1107be6cb4935397e6bbf5732cb65f3296
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-09-12 22:29:46 +0800

    Fix screen size responsiveness

--- src/components/BottomLogo.js
+++ src/components/BottomLogo.js
@@ -1,14 +1,17 @@
+'use client'
 import React from 'react'
 import Logo from './Logo'
 
-function BottomLogo() {
+function BottomLogo({ occurrence }) {
+  const array = [...Array(occurrence)]
+
   return (
-    <div className=' h-fit w-[90%] inset-x-0 absolute bottom-0 mb-[-10px] grid-cols-2 md:grid-cols-3 grid lg:grid-cols-5 gap-8 bg-white p-16 rounded-[30px] mx-auto drop-shadow-[0px_20px_60px_rgba(0, 0, 0, 0.03)]'>
-        <Logo bColor='text-white' nameColor='text-[#3C414E]'/>
-        <Logo bColor='text-white' nameColor='text-[#3C414E]'/>
-        <Logo bColor='text-white' nameColor='text-[#3C414E]'/>
-        <Logo bColor='text-white' nameColor='text-[#3C414E]'/>
-        <Logo bColor='text-white' nameColor='text-[#3C414E]'/>
+    <div className=' h-fit w-[90%] inset-x-0 absolute bottom-0 mb-[-10px] grid grid-cols-1 md:grid-cols-3  lg:grid-cols-5 gap-8 bg-white p-16 rounded-[30px] mx-auto drop-shadow-[0px_20px_60px_rgba(0, 0, 0, 0.03)]'>
+      {console.log(occurrence)}
+
+      {
+        array.map( ()=> <Logo bColor='text-white' nameColor='text-[#3C414E]'/>)  
+      }
         
 
     </div>
