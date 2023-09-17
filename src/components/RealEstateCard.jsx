acd2f178d7c62b3b132fcd030012464afb435013
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-09-18 02:00:17 +0800

    Fix the real estate card style

--- src/components/RealEstateCard.jsx
+++ src/components/RealEstateCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Manrope } from "next/font/google";
 import Image from "next/image";
 
@@ -12,33 +12,44 @@ function RealEstateCard({
   price,
   imageSRC,
 }) {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    return setLoading(false);
+  }, []);
   return (
-    <div
-      className={`w-fit h-fit relative ${translateYNM} ${translateYMD} ${translateYLG} font-${manrope.className}`}
-    >
-      <Image
-        width="20000"
-        height="20000"
-        className="w-[366px] h-[400px] object-cover object-center rounded-[20px] shadow-[0px_4px_120px_rgba(58, 86, 78, 0.07)]"
-        src={imageSRC}
-        alt="Mixtownia-image"
-      />
+    <>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div
+          className={`w-fit h-fit relative ${translateYNM} ${translateYMD} ${translateYLG} font-${manrope.className}`}
+        >
+          <Image
+            width="20000"
+            height="20000"
+            className="w-[366px] h-[400px] object-cover object-center rounded-[20px] shadow-[0px_4px_120px_rgba(58, 86, 78, 0.07)]"
+            src={imageSRC}
+            alt="Mixtownia-image"
+          />
 
-      <div className="rounded-[20px] bg-white flex flex-row absolute inset-x-0 md:inset-x-[5%] lg:inset-x-[5%] bottom-0 -mb-0 md:-mb-[50px] lg:-mb-[50px] justify-evenly items-center p-5 shadow-lg">
-        {/* shadow-[0px_4px_120px_0px_rgba(58, 86, 78, 0.20)] */}
-        <div>
-          <h2 className="text-2xl font-bold leading-[180%] text-[#3C414E">
-            {name}
-          </h2>
-          <p className="text-xl font-extrabold leading-[180%] text-[#6663E8] drop-shadow-[0px_4px_60px_rgba(0, 0, 0, 0.05)]">
-            {price}
-          </p>
+          <div className="rounded-[20px] bg-white flex flex-row absolute inset-x-0 md:inset-x-[5%] lg:inset-x-[5%] bottom-0 -mb-0 md:-mb-[50px] lg:-mb-[50px] justify-evenly items-center p-5 shadow-lg">
+            {/* shadow-[0px_4px_120px_0px_rgba(58, 86, 78, 0.20)] */}
+            <div>
+              <h2 className="text-2xl font-bold leading-[180%] text-[#3C414E">
+                {name}
+              </h2>
+              <p className="text-xl font-extrabold leading-[180%] text-[#6663E8] drop-shadow-[0px_4px_60px_rgba(0, 0, 0, 0.05)]">
+                {price}
+              </p>
+            </div>
+            <button className="w-[114px] h-[52px] rounded-[30px] font-extrabold leading-[180%] text-sm text-[#fff] bg-[#6663E9]">
+              Detail
+            </button>
+          </div>
         </div>
-        <button className="w-[114px] h-[52px] rounded-[30px] font-extrabold leading-[180%] text-sm text-[#fff] bg-[#6663E9]">
-          Detail
-        </button>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
--- src/sections/RealEstates.jsx
+++ src/sections/RealEstates.jsx
@@ -1,9 +1,110 @@
-import React, { useEffect, useState } from "react";
 import { Manrope } from "next/font/google";
 import RealEstateCard from "@/components/RealEstateCard";
-import realEstates from "../data/realEstates.json";
+// import realEstates from "../data/realEstates.json";
 
 const manrope = Manrope({ subsets: ["latin"] });
+
+const realEstates = [
+  {
+    id: 0,
+    translateYNM: "translate-y-0",
+    translateYMD: "md:translate-y-0",
+    translateYLG: "lg:translate-y-0",
+    price: "$205,500",
+    name: "Mixtownia",
+    imageSRC:
+      "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Mixtownia",
+  },
+  {
+    id: 1,
+    translateYNM: "translate-y-0",
+    translateYMD: "md:translate-y-[30px]",
+    translateYLG: "lg:translate-y-[30px]",
+    price: "$102,200",
+    name: "Mirtawan",
+    imageSRC:
+      "https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Mixtownia",
+  },
+  {
+    id: 2,
+    translateYNM: "translate-y-0",
+    translateYMD: "md:translate-y-[60px]",
+    translateYLG: "lg:translate-y-[60px]",
+    price: "$135,900",
+    name: "Wartawan",
+    imageSRC:
+      "https://images.pexels.com/photos/280222/pexels-photo-280222.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Mixtownia",
+  },
+  {
+    id: 3,
+    translateYNM: "translate-y-0",
+    translateYMD: "md:translate-y-[60px]",
+    translateYLG: "lg:translate-y-[60px]",
+    price: "$205,500",
+    name: "Mixtownia",
+    imageSRC:
+      "https://images.pexels.com/photos/164522/pexels-photo-164522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Mixtownia",
+  },
+  {
+    id: 4,
+    translateYNM: "translate-y-0",
+    translateYMD: "md:translate-y-[30px]",
+    translateYLG: "lg:translate-y-[30px]",
+    price: "$205,500",
+    name: "Mixtownia",
+    imageSRC:
+      "https://images.pexels.com/photos/53610/large-home-residential-house-architecture-53610.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Mixtownia",
+  },
+  {
+    id: 5,
+    translateYNM: "translate-y-0",
+    translateYMD: "md:translate-y-0",
+    translateYLG: "lg:translate-y-0",
+    price: "$205,500",
+    name: "Mixtownia",
+    imageSRC:
+      "https://images.pexels.com/photos/209296/pexels-photo-209296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Mixtownia",
+  },
+  {
+    id: 6,
+    translateYNM: "translate-y-0",
+    translateYMD: "md:translate-y-0",
+    translateYLG: "lg:translate-y-0",
+    price: "$205,500",
+    name: "Mixtownia",
+    imageSRC:
+      "https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Mixtownia",
+  },
+  {
+    id: 7,
+    translateYNM: "translate-y-0",
+    translateYMD: "md:translate-y-[30px]",
+    translateYLG: "lg:translate-y-[30px]",
+    price: "$205,500",
+    name: "Mixtownia",
+    imageSRC:
+      "https://images.pexels.com/photos/1212053/pexels-photo-1212053.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Mixtownia",
+  },
+  {
+    id: 8,
+    translateYNM: "translate-y-0",
+    translateYMD: "md:translate-y-[60px]",
+    translateYLG: "lg:translate-y-[60px]",
+    price: "$205,500",
+    name: "Mixtownia",
+    imageSRC:
+      "https://images.pexels.com/photos/3935333/pexels-photo-3935333.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Mixtownia",
+  },
+];
 
 function RealEstates() {
   return (
