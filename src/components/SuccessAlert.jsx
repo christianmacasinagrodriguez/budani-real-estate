4c4a9fb2adddcf34d1f5245ee643dd9777dcf90c
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-09-18 20:19:33 +0800

    Create email functionalities

--- src/components/SuccessAlert.jsx
+++ src/components/SuccessAlert.jsx
@@ -9,7 +9,7 @@ const SuccessAlert = ({ heading, text, alertView }) => {
     <div
       className={`fixed inset-0 w-full h-full bg-[#00000085] ${alertView} flex-col justify-center items-center font-${manrope.className} z-40`}
     >
-      <div className="relative w-fit h-fit p-20 bg-white rounded-lg flex flex-col gap-2 justify-center">
+      <div className="relative w-fit h-fit p-20 bg-white mx-2 rounded-lg flex flex-col gap-2 justify-center">
         <div className=" absolute top-0 inset-x-0 mx-auto -mt-[60px]  p-5 bg-[#6663E9] w-fit h-fit flex flex-col justify-center items-center rounded-full text-white text-7xl">
           <FaCheck />
         </div>
